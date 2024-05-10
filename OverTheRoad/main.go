565d0d3021f6a6a4d5cf0a23f6d19f50813c8f92
Danil Komov <komovdi88@gmail.com>
2024-05-10 14:25:45 +0300

    Over the Road

--- OverTheRoad/main.go
+++ OverTheRoad/main.go
@@ -2,25 +2,31 @@ package main
 
 import "fmt"
 
+// my code @no optimize :(
+// func OverTheRoad(address int, n int) int {
+// 	i, j := n, 0
+// 	street := make(map[int]int)
+// 	for i > 0 && j < n {
+// 		street[2*i] = j*2 + 1
+// 		i--
+// 		j++
+// 	}
+// 	fmt.Println(street)
+// 	for key, val := range street {
+// 		if val == address {
+// 			return key
+// 		}
+// 	}
+// 	return street[address]
+// }
+
+// computer code
 func OverTheRoad(address int, n int) int {
-	i, j := n, 0
-	street := make(map[int]int)
-	for i > 0 && j < n {
-		street[2*i] = j*2 + 1
-		i--
-		j++
-	}
-	fmt.Println(street)
-	for key, val := range street {
-		if val == address {
-			return key
-		}
-	}
-	return street[address]
+	return 2*n - address + 1
 }
 
 func main() {
-	fmt.Println(OverTheRoad(1, 3)) //6
+	fmt.Println(OverTheRoad(6, 4)) //6
 }
 
 // 1|   |8
