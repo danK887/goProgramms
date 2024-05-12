fd2b66025f7601f030a4e21c716133498d2416a4
Danil Komov <komovdi88@gmail.com>
2024-05-12 14:14:43 +0300

    Consecutive strings

---
+++ LongestConsec/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func LongestConsec(strarr []string, k int) string {
+	// your code
+}
+
+func main() {
+	fmt.Println(LongestConsec([]string{"zone", "abigail", "theta", "form", "libe", "zas"}, 2)) // "abigailtheta"
+}
