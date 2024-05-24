df58f734f908084b653b4a5607e166ed33bd3030
Danil Komov <komovdi88@gmail.com>
2024-05-24 16:19:42 +0300

    If you can read ...

--- NATOAlphabet/main.go
+++ NATOAlphabet/main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func ToNato(words string) string {
-	alphabet := map[string]string{"A": "Alfa", "B": "Bravo", "C": "Charlie", "D": "Delta", "E": "Echo", "F": "Foxtrot", "G": "Golf", "H": "Hotel", "I": "India", "J": "Juliett", "K": "Kilo", "L": "Lima", "M": "Mike", "N": "November", "O": "Oscar", "P": "Papa", "Q": "Quebec", "R": "Romeo", "S": "Sierra", "T": "Tango", "U": "Uniform", "V": "Victor", "W": "Whiskey", "X": "Xray", "Y": "Yankee", "Z": "Zulu"}
+	alphabet := map[string]string{"A": "Alfa", "B": "Bravo", "C": "Charlie", "D": "Delta",
+		"E": "Echo", "F": "Foxtrot", "G": "Golf", "H": "Hotel", "I": "India",
+		"J": "Juliett", "K": "Kilo", "L": "Lima", "M": "Mike", "N": "November", "O": "Oscar",
+		"P": "Papa", "Q": "Quebec", "R": "Romeo", "S": "Sierra", "T": "Tango",
+		"U": "Uniform", "V": "Victor", "W": "Whiskey", "X": "Xray", "Y": "Yankee", "Z": "Zulu"}
 	trimmedStr := strings.ReplaceAll(words, " ", "")
 	strToConvert := strings.ToUpper(trimmedStr)
 	wordArray := []string{}
