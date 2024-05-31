8f90ce368d69672d11ac3b3e5cafb683552b08db
Danil Komov <komovdi88@gmail.com>
2024-05-31 18:17:11 +0300

    Char Code Calculation

--- CharCodeCalculation/main.go
+++ CharCodeCalculation/main.go
@@ -1,16 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func Calc(s string) int {
-	sum := 0
+	total1, val := "", ""
+	lst1 := []string{}
+	lst2 := []string{}
+	sum1, sum2 := 0, 0
+	gg := 0
 	for _, v := range s {
-		sum += int(v)
+		gg = int(v)
+		val = strconv.Itoa(gg)
+		total1 = total1 + val
 	}
-	fmt.Println(sum)
-	return 0
+
+	for _, val := range total1 {
+		lst1 = append(lst1, string(val))
+	}
+
+	for _, val := range lst1 {
+		if val != "7" {
+			lst2 = append(lst2, val)
+		} else {
+			lst2 = append(lst2, "1")
+		}
+	}
+
+	for _, val := range lst1 {
+		gg, _ = strconv.Atoi(val)
+		sum1 = sum1 + gg
+	}
+
+	for _, val := range lst2 {
+		gg, _ = strconv.Atoi(val)
+		sum2 = sum2 + int(gg)
+	}
+	return sum1 - sum2
 }
 
 func main() {
-	fmt.Println(Calc("abcdef")) //6
+	fmt.Println(Calc("aaaaaddddr")) //6
 }
