7868e518db21abd370443b2974770fd9c10ed76c
Danil Komov <komovdi88@gmail.com>
2024-06-04 17:14:26 +0300

    Next bigger number with the same digits

--- NextBiggerNumberWithTheSameDigits/main.go
+++ NextBiggerNumberWithTheSameDigits/main.go
@@ -1,12 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func NextBigger(n int) int {
-	// your code here
+	new := strconv.Itoa(n)
+	if len(new) == 1 {
+		return -1
+	}
+	total := ""
+	result := 0
+	lst := []string{}
+	for _, v := range new {
+		lst = append(lst, string(v))
+	}
+	for i := len(lst) - 1; i > 0; i-- {
+		if lst[i] > lst[i-1] {
+			lst[i], lst[i-1] = lst[i-1], lst[i]
+			total = strings.Join(lst, "")
+			result, _ = strconv.Atoi(total)
+			return result
+		}
+	}
 	return -1
 }
 
 func main() {
-	fmt.Println(NextBigger(123))
+	fmt.Println(NextBigger(22))
 }
