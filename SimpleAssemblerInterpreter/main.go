e8a83922dd3dd58d45b22b0bb8ab60d76e140e3a
Danil Komov <komovdi88@gmail.com>
2024-06-05 14:22:54 +0300

    Simple assembler interpreter

--- SimpleAssemblerInterpreter/main.go
+++ SimpleAssemblerInterpreter/main.go
@@ -9,32 +9,40 @@ import (
 func SimpleAssembler(program []string) map[string]int {
 	value := make(map[string]int)
 	instrDecomp := []string{}
+	var err error
 	for i := 0; i < len(program); i++ {
 		instruction := program[i]
 		instrDecomp = strings.Fields(instruction)
 		for _, val := range instrDecomp {
 			switch val {
 			case "mov":
-				value[instrDecomp[1]], _ = strconv.Atoi(instrDecomp[2])
+				value[instrDecomp[1]], err = strconv.Atoi(instrDecomp[2])
+				if err != nil {
+					value[instrDecomp[1]] = value[instrDecomp[2]]
+				}
 			case "inc":
 				value[instrDecomp[1]]++
 			case "dec":
 				value[instrDecomp[1]]--
-				// case "jnz":
-				// 	if value[instrDecomp[1]] == 0 {
-				// 		break
-				// 	} else if instrDecomp[2] == "-1" {
-
-				// 	}
+			case "jnz":
+				instr, _ := strconv.Atoi(instrDecomp[2])
+				if value[instrDecomp[1]] == 0 {
+					break
+				} else if instr < 0 {
+					i = i + instr - 1
+					break
+				} else if instr > 0 {
+					i = i + instr - 1
+					break
+				}
 			}
 			break
 		}
 	}
-
-	fmt.Println(instrDecomp)
 	return value
 }
 
 func main() {
-	fmt.Println(SimpleAssembler([]string{"mov a 1", "inc a", "dec a", "dec a", "jnz a -1", "inc a"})) //{"a": 1}
+	fmt.Println(SimpleAssembler([]string{"mov a -10", "mov b a", "inc a", "dec b", "jnz a -2"})) //{"a": 1}
+
 }
